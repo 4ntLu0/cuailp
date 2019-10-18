dfd2be65266923c4dff95d95c82f545a40778d66
Anthony Luo <anthonyluo100@gmail.com>
2019-10-18 11:13:35 -0400

    Update File Input
    Added *colours* and *formatting* to the file output (as well as the class). 
    
    Cv2 only works with / directory names, so updated that. Need to find a better way to change that.

--- license_plate_py/Test.py
+++ license_plate_py/Test.py
@@ -1,19 +1,38 @@
 import cv2
 import matplotlib.pyplot as plt
 import os
+from pathlib import Path
+
+class colour:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
 
 img_matrix = []
-for filename in os.listdir("F:\\Cuyow\\cuailp\\license_plate_py\\testing_imgs"):
+mainPath = 'F:/Cuyow/cuailp/license_plate_py/test_img/'
+for filename in os.listdir("F:/Cuyow/cuailp/license_plate_py/test_img"):
+    currentPath = mainPath + filename
+    print('Working with: ', colour.BOLD + colour.BLUE + filename + colour.END, 'which has path: ', colour.BOLD + colour.BLUE + currentPath + colour.END)
+    print(cv2.imread(currentPath), '\n')
     img_matrix.append(cv2.imread(filename))
-    print(cv2.imread(filename))
 
     # Why is it returning none type for the last image?
-
+'''
     print(filename)
     cv2.namedWindow('Window')
     cv2.imshow('Window', cv2.imread(filename))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    '''
+
+exit()
 
 for i in range(len(img_matrix) - 1):
     windowName = 'Window' + str(i)
