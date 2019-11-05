2b14ce9721ab56ce0d9791863905f735d79c79ff
Anthony Luo <anthonyluo100@gmail.com>
2019-11-05 12:06:33 -0500

    Added loadWeights

--- license_plate_py/Yolov3_TF2/convert_weight.py
+++ license_plate_py/Yolov3_TF2/convert_weight.py
@@ -4,4 +4,4 @@ import tensorflow as tf
 import numpy as np
 
 from model import yolov3
-form utils.misc_utils import parse_anchors, load_weights
+form utils.misc_utils import parseAchors, loadWeights
