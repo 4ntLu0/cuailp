6fe10f0b0aafd8448d581056c1bdf40bbc0be357
Anthony Luo <anthonyluo100@gmail.com>
2019-11-08 08:57:50 -0500

    convs

--- license_plate_py/Yolov3_TF2/convert_weight.py
+++ license_plate_py/Yolov3_TF2/convert_weight.py
@@ -9,10 +9,20 @@ from args import getWeights, getSavePath, getAnchors
 
 num_class = 80
 img_size = 416
-weight_paths = getWeights()
+weight_path = getWeights()
 save_path = getSavePath()
 anchors = getAnchors()
 
 model = yolov3(80, anchors)
-with tf.session() as sess:
-    
+with tf.Session() as sess:
+    inputs = tf.placeholder(tf.float32, [1, img_size, img_size, 3])
+
+    with tf.variable_scope('yolov3'):
+        feature_map = model.forward(inputs)
+
+    saver = tf.train.saver(var_list=tf.global_variables(scope='yolov3'))
+
+    load_ops = loadWeights(tf.global_variables(scope = 'yolov3'), weight_path)
+    sess.run(load_ops)
+    saver.save(sess, save_path=save_path)
+    print('Tensorflow model checkpoint has been saved to {}'.format(save_path))
